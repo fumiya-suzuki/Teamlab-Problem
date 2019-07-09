ba3e4a5803f4683b012e63a482ecc3b683218a50
fumiya-suzuki <fumiya.89.1216@gmail.com>
2019-07-09 07:31:05 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    
+    def full_title(page_name = "")
+        base_title = "TeamlabApp"
+        if page_name.empty?
+            base_title
+        else
+            page_name + " | " + basetitle
+        end
+    end
 end
